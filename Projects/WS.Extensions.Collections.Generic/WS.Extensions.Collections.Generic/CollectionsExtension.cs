1b7440ec1de309676923fb2186e054ab123a059a
Wagsn <wagsn@foxmail.com>
2020-01-08 11:44:33 +0800

    Add WS.Extensions.Collections.Generic Project

---
+++ Projects/WS.Extensions.Collections.Generic/WS.Extensions.Collections.Generic/CollectionsExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WS.Extensions.Collections.Generic
+{
+    /// <summary>
+    /// System.Collections.Generic Extension
+    /// </summary>
+    public static class CollectionsExtension
+    {
+        public static void Refresh<T>(this IList<T> list, IEnumerable<T> other)
+        {
+            foreach(var item in other)
+            {
+                list.Add(item);
+            }
+        }
+    }
+}
