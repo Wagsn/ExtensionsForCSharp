e92e139bba261b512f9be68712feb1c52c643e9e
Wagsn <wagsn@foxmail.com>
2020-01-08 12:56:38 +0800

    Update CollectionsExtension.cs

--- Projects/WS.Extensions.Collections.Generic/WS.Extensions.Collections.Generic/CollectionsExtension.cs
+++ Projects/WS.Extensions.Collections.Generic/WS.Extensions.Collections.Generic/CollectionsExtension.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace WS.Extensions.Collections.Generic
+﻿namespace System.Collections.Generic
 {
     /// <summary>
     /// System.Collections.Generic Extension
     /// </summary>
     public static class CollectionsExtension
     {
+        /// <summary>
+        /// Clear this list and AddRange other
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="other"></param>
         public static void Refresh<T>(this IList<T> list, IEnumerable<T> other)
         {
+            list.Clear();
             foreach(var item in other)
             {
                 list.Add(item);
