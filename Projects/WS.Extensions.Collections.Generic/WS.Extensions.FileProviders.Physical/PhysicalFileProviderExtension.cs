606e999bb3644e7bc46fbae6fd5232b0aaeb1e84
Wagsn <wagsn@foxmail.com>
2020-01-08 15:23:44 +0800

    Update

---
+++ Projects/WS.Extensions.Collections.Generic/WS.Extensions.FileProviders.Physical/PhysicalFileProviderExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Extensions.FileProviders
+{
+    /// <summary>
+    /// FileProvider Extension
+    /// </summary>
+    public static class PhysicalFileProviderExtension
+    {
+        public static IEnumerable<IFileInfo> GetDirectoryDepthContents(this PhysicalFileProvider fileProvider, string subpath)
+        {
+            var rootPath = fileProvider.Root;
+            List<IFileInfo> fileInfos = new List<IFileInfo>();
+            var files = fileProvider.GetDirectoryContents(subpath).ToList();
+            foreach (var dir in files.Where(a => a.IsDirectory))
+            {
+                fileInfos.AddRange(GetDirectoryDepthContents(fileProvider, dir.PhysicalPath.Substring(rootPath.Length)));
+            }
+            fileInfos.AddRange(files);
+            return fileInfos;
+        }
+    }
+}
---
+++ Projects/WS.Extensions.Collections.Generic/WS.Extensions.FileProviders.Physical/README.md
@@ -0,0 +1,3 @@
+﻿# PhysicalFileProviderExtension
+
+PhysicalFileProvider Extension
