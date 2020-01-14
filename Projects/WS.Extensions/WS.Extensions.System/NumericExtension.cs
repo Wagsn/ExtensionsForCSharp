c30240102805d285d18fedd91d5ad3d8a0ef4e56
Wagsn <wagsn@foxmail.com>
2020-01-14 15:08:25 +0800

    Add WS.Extensions.System Project

---
+++ Projects/WS.Extensions/WS.Extensions.System/NumericExtension.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace WS.Extensions.System
+//{
+//    /// <summary>
+//    /// 数值类型扩展
+//    /// </summary>
+//    public static class NumericExtension
+//    {
+//        /// <summary>
+//        /// 转换为中文数值字符串
+//        /// </summary>
+//        /// <param name="input"></param>
+//        /// <returns></returns>
+//        public static string ToChinese(this int input)
+//        {
+//            return ToChinese((long)input);
+//        }
+//        /// <summary>
+//        /// 转换为中文数值字符串
+//        /// </summary>
+//        /// <param name="input"></param>
+//        /// <returns></returns>
+//        public static string ToChinese(this long input)
+//        {
+
+//            return input.ToString();
+//        }
+//    }
+//}
